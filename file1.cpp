4feac7934a31f48e1bc25474f25269cc2142e54c
Rayen1a <raihan01426@gmail.com>
2025-05-23 08:40:21 +0700

    memasukkan case 4 cout & break

--- file1.cpp
+++ file1.cpp
@@ -119,6 +119,10 @@ int main()
                 cout << "Stack is Empty.No top value" << endl;
             }
             break;
+
+            case 4:
+            cout << "Exiting program." << endl;
+            break;
         }
     }
 }
