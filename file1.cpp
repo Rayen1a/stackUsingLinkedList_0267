fd917398844b1f3af3f946141b59e3709c1318ca
Rayen1a <raihan01426@gmail.com>
2025-05-23 07:54:53 +0700

    memasukkan class private

--- file1.cpp
+++ file1.cpp
@@ -12,3 +12,15 @@ class Node
         next = NULL;
     }
 };
+// Stack class
+class Stack
+{
+    private:
+    Node *top; // Pointer to the top node of the stack
+
+    public:
+    Stack()
+    {
+        top = NULL; // Initialize the stack with a null top pointer
+    }
+}    
