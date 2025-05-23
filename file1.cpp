52f24854814c9b34ce9401f6938617c28bec01ff
Rayen1a <raihan01426@gmail.com>
2025-05-23 08:24:34 +0700

    case 2 stack.isempty

--- file1.cpp
+++ file1.cpp
@@ -97,6 +97,12 @@ int main()
             cin >> value;
             stack.push(value); // push the entered value onto the stack
             break;
+
+            case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
         }
     }
 }
